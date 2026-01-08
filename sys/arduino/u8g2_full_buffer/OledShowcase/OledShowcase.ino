08f8a3325d3e449ec48b995cf3e611ea09e6ede5
Renato <renato.troncone91@gmail.com>
2026-01-09 00:26:45 +0100

    Refine OLED WiFi showcase navigation

--- sys/arduino/u8g2_full_buffer/OledShowcase/OledShowcase.ino
+++ sys/arduino/u8g2_full_buffer/OledShowcase/OledShowcase.ino
@@ -2,16 +2,18 @@
   OledShowcase.ino
 
   Demo "bello" con tre pulsanti per Arduino + OLED SSD1309 128x64.
-  Schermate: Orologio, Onda, Starfield, Diagnostica.
+  Schermate: Orologio, Onda, WiFi Scanner, WiFi Info.
   Pulsanti (con pull-up interno):
     PREV  -> D2
     NEXT  -> D3
-    SELECT-> D4 (inverte i colori)
+    SELECT-> D4 (azione contestuale / inverti colori)
 */
 
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <math.h>
+#include <WiFiS3.h>
+#include <string.h>
 
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
@@ -25,6 +27,16 @@ constexpr uint8_t kBtnSel  = 4;
 
 constexpr uint8_t kPages = 4;
 constexpr unsigned long kDebounceMs = 40;
+constexpr unsigned long kWifiRescanMs = 15000;
+
+constexpr int kWifiMaxNetworks = 10;
+char wifiSsid[kWifiMaxNetworks][33];
+int wifiRssi[kWifiMaxNetworks];
+bool wifiEncrypted[kWifiMaxNetworks];
+int wifiCount = 0;
+
+const char *kKnownSsid = "Casa";
+const char *kKnownPass = "04071991";
 
 struct ButtonState {
   uint8_t pin;
@@ -39,6 +51,12 @@ ButtonState btnSel{ kBtnSel,  false, false, 0 };
 
 uint8_t page = 0;
 bool invertColors = false;
+bool wifiConnected = false;
+bool wifiConnecting = false;
+unsigned long wifiLastScan = 0;
+unsigned long wifiConnectStart = 0;
+int wifiSelected = 0;
+int wifiStatus = WL_IDLE_STATUS;
 
 unsigned long lastSecond = 0;
 int seconds = 0;
@@ -48,15 +66,6 @@ int hours   = 12;
 unsigned long frameTimer = 0;
 uint16_t frameCount = 0;
 uint16_t fps = 0;
-
-struct Star {
-  float x;
-  float y;
-  float speed;
-};
-
-constexpr uint8_t kStarCount = 18;
-Star stars[kStarCount];
 
 bool wasPressed(ButtonState &btn) {
   bool reading = (digitalRead(btn.pin) == LOW);
@@ -98,24 +107,6 @@ void updateFps() {
   }
 }
 
-void initStars() {
-  for (uint8_t i = 0; i < kStarCount; i++) {
-    stars[i].x = random(0, 128);
-    stars[i].y = random(0, 64);
-    stars[i].speed = 0.6f + (random(0, 100) / 100.0f);
-  }
-}
-
-void updateStars() {
-  for (uint8_t i = 0; i < kStarCount; i++) {
-    stars[i].x -= stars[i].speed;
-    if (stars[i].x < 0) {
-      stars[i].x = 127;
-      stars[i].y = random(0, 64);
-      stars[i].speed = 0.6f + (random(0, 100) / 100.0f);
-    }
-  }
-}
 
 void drawHeader(const char *title) {
   u8g2.setFont(u8g2_font_6x12_tf);
@@ -155,17 +146,6 @@ void drawWavePage() {
   }
 }
 
-void drawStarfieldPage() {
-  drawHeader("Starfield");
-  updateStars();
-  for (uint8_t i = 0; i < kStarCount; i++) {
-    u8g2.drawPixel((int)stars[i].x, (int)stars[i].y);
-  }
-
-  u8g2.drawFrame(0, 16, 128, 48);
-  u8g2.drawHLine(0, 40, 128);
-}
-
 void drawInfoPage() {
   drawHeader("Diagnostica");
   u8g2.setFont(u8g2_font_6x12_tf);
@@ -184,6 +164,103 @@ void drawInfoPage() {
   u8g2.drawStr(0, 58, line);
 }
 
+void wifiScan() {
+  wifiCount = 0;
+  int found = WiFi.scanNetworks();
+  if (found <= 0) {
+    return;
+  }
+  wifiCount = min(found, kWifiMaxNetworks);
+  for (int i = 0; i < wifiCount; i++) {
+    String ssid = WiFi.SSID(i);
+    ssid.toCharArray(wifiSsid[i], sizeof(wifiSsid[i]));
+    wifiRssi[i] = WiFi.RSSI(i);
+    wifiEncrypted[i] = (WiFi.encryptionType(i) != ENC_TYPE_NONE);
+  }
+}
+
+void wifiStartConnect() {
+  wifiConnecting = true;
+  wifiConnectStart = millis();
+  WiFi.disconnect();
+  WiFi.begin(kKnownSsid, kKnownPass);
+}
+
+void wifiUpdateStatus() {
+  wifiStatus = WiFi.status();
+  wifiConnected = (wifiStatus == WL_CONNECTED);
+  if (wifiConnecting && wifiConnected) {
+    wifiConnecting = false;
+  }
+  if (wifiConnecting && (millis() - wifiConnectStart > 15000)) {
+    wifiConnecting = false;
+  }
+}
+
+void drawWifiPage() {
+  drawHeader("WiFi Scanner");
+  u8g2.setFont(u8g2_font_5x8_tf);
+
+  if (wifiCount == 0) {
+    u8g2.drawStr(0, 28, "Nessuna rete trovata");
+    u8g2.drawStr(0, 40, "PREV/NEXT: pagine");
+    u8g2.drawStr(0, 52, "SEL: forza scan");
+    return;
+  }
+
+  int start = (wifiSelected / 4) * 4;
+  int y = 26;
+  for (int i = start; i < min(start + 4, wifiCount); i++) {
+    if (i == wifiSelected) {
+      u8g2.drawBox(0, y - 7, 128, 9);
+      u8g2.setDrawColor(invertColors ? 1 : 0);
+    }
+    char line[32];
+    const char *lock = wifiEncrypted[i] ? "*" : " ";
+    snprintf(line, sizeof(line), "%s %s %ddBm", lock, wifiSsid[i], wifiRssi[i]);
+    u8g2.drawStr(2, y, line);
+    if (i == wifiSelected) {
+      u8g2.setDrawColor(invertColors ? 0 : 1);
+    }
+    y += 10;
+  }
+
+  u8g2.setFont(u8g2_font_5x8_tf);
+  if (strcmp(wifiSsid[wifiSelected], kKnownSsid) == 0) {
+    u8g2.drawStr(0, 62, "SEL: connetti Casa");
+  } else {
+    u8g2.drawStr(0, 62, "SEL: nuova scansione");
+  }
+}
+
+void drawWifiInfoPage() {
+  drawHeader("WiFi Info");
+  u8g2.setFont(u8g2_font_5x8_tf);
+
+  char line[32];
+  if (wifiConnected) {
+    IPAddress ip = WiFi.localIP();
+    snprintf(line, sizeof(line), "SSID: %s", WiFi.SSID().c_str());
+    u8g2.drawStr(0, 26, line);
+    snprintf(line, sizeof(line), "IP: %u.%u.%u.%u", ip[0], ip[1], ip[2], ip[3]);
+    u8g2.drawStr(0, 36, line);
+    snprintf(line, sizeof(line), "RSSI: %ddBm", WiFi.RSSI());
+    u8g2.drawStr(0, 46, line);
+    snprintf(line, sizeof(line), "CH: %d", WiFi.channel());
+    u8g2.drawStr(0, 56, line);
+    return;
+  }
+
+  if (wifiConnecting) {
+    u8g2.drawStr(0, 30, "Connessione...");
+    u8g2.drawStr(0, 42, kKnownSsid);
+    return;
+  }
+
+  u8g2.drawStr(0, 30, "Non connesso");
+  u8g2.drawStr(0, 42, "SEL: connetti Casa");
+}
+
 void setup() {
   u8g2.begin();
   u8g2.setBusClock(8000000);
@@ -193,23 +270,52 @@ void setup() {
   pinMode(kBtnSel, INPUT_PULLUP);
 
   randomSeed(micros());
-  initStars();
   frameTimer = millis();
   lastSecond = millis();
+  wifiLastScan = 0;
+  WiFi.setHostname("u8g2-r4");
 }
 
 void loop() {
   updateClock();
   updateFps();
+  wifiUpdateStatus();
+
+  if (millis() - wifiLastScan >= kWifiRescanMs) {
+    wifiLastScan = millis();
+    wifiScan();
+  }
 
   if (wasPressed(btnPrev)) {
-    page = (page + kPages - 1) % kPages;
+    if (page == 2 && wifiCount > 0) {
+      wifiSelected = (wifiSelected + wifiCount - 1) % wifiCount;
+    } else {
+      page = (page + kPages - 1) % kPages;
+    }
   }
   if (wasPressed(btnNext)) {
-    page = (page + 1) % kPages;
+    if (page == 2 && wifiCount > 0) {
+      wifiSelected = (wifiSelected + 1) % wifiCount;
+    } else {
+      page = (page + 1) % kPages;
+    }
   }
   if (wasPressed(btnSel)) {
-    invertColors = !invertColors;
+    if (page == 3) {
+      if (!wifiConnecting && !wifiConnected) {
+        wifiStartConnect();
+      }
+    } else if (page == 2) {
+      if (wifiCount == 0) {
+        wifiScan();
+      } else if (strcmp(wifiSsid[wifiSelected], kKnownSsid) == 0) {
+        wifiStartConnect();
+      } else {
+        wifiScan();
+      }
+    } else {
+      invertColors = !invertColors;
+    }
   }
 
   u8g2.clearBuffer();
@@ -223,10 +329,10 @@ void loop() {
       drawWavePage();
       break;
     case 2:
-      drawStarfieldPage();
+      drawWifiPage();
       break;
     default:
-      drawInfoPage();
+      drawWifiInfoPage();
       break;
   }
 
